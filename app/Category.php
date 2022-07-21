f057014f130a47a252de2c1e970cc815cc885c1d
P4TTANAPON <pattanaponn@gmail.com>
2022-07-21 20:55:45 +0700

    rename catagory > category && make category form

--- app/Category.php
+++ app/Category.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Catagory extends Model
+class Category extends Model
 {
     //
 }
--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class CatagoryController extends Controller
+class CategoryController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -13,7 +13,7 @@ class CatagoryController extends Controller
      */
     public function index()
     {
-        return view('catagories.index');
+        return view('categories.index');
     }
 
     /**
@@ -23,7 +23,7 @@ class CatagoryController extends Controller
      */
     public function create()
     {
-        //
+        return view('categories.create');
     }
 
     /**
@@ -34,7 +34,7 @@ class CatagoryController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        dd($request->all());
     }
 
     /**
