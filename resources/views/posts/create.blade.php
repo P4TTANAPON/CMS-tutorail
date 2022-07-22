d4d45a363505f639dd8e0235f80d4fbc0c441da3
P4TTANAPON <pattanaponn@gmail.com>
2022-07-22 21:40:44 +0700

    edit guest content & making view post

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app');
+@section('content')
+    <div class="card card-default">
+        <div class="card-header">
+            Create Post
+        </div>
+        <div class="card-body">
+            
+        </div>
+    </div>
+@endsection
+
+
