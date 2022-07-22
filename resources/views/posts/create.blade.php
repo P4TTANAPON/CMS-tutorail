35f3b58e95046956cf30a66b15af0c56ea750c21
P4TTANAPON <pattanaponn@gmail.com>
2022-07-22 21:51:04 +0700

    form post

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -5,7 +5,28 @@
             Create Post
         </div>
         <div class="card-body">
-            
+            <form action="{{route('posts.store')}}" method="post">
+                @csrf
+                <div class="form-group">
+                    <label for="title">Title</label>
+                    <input type="text" name="title" value="" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="description">Description</label>
+                    <textarea name="description" id="" cols="4" rows="4" class="form-control"></textarea>
+                </div>
+                <div class="form-group">
+                    <label for="content">Content</label>
+                    <textarea name="content" id="" cols="8" rows="8" class="form-control"></textarea>
+                </div>
+                <div class="form-group">
+                    <label for="image">Image</label>
+                    <input type="file" name="image" id="" class="form-control">
+                </div>
+                <div class="form-group">
+                    <input type="submit" value="Create Post" class="btn btn-success">
+                </div>
+            </form>
         </div>
     </div>
 @endsection
