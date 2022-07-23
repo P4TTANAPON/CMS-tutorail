9996b60ef63e6b061de64b642dc51cf9639aa5ea
P4TTANAPON <pattanaponn@gmail.com>
2022-07-23 23:15:47 +0700

    add tag

---
+++ resources/views/tags/index.blade.php
@@ -0,0 +1,57 @@
+@extends('layouts.app');
+@section('content')
+    <div class="d-flex justify-content-end mb-2">
+        <a href="{{route('tags.create')}}" class="btn btn-success">Add Tag</a>
+    </div>
+    <div class="card card-default">
+        <div class="card-header">
+            Tag
+        </div>
+        <div class="card-body">
+            @if ($tags->count()>0)
+                <table class="table">
+                    <thead>
+                        <th>Name</th>
+                        {{-- <th>Post Counts</th> --}}
+                        <th></th>
+                        <th></th>
+                    </thead>
+                    <tbody>
+                        @foreach ($tags as $tag)
+                            <tr>
+                                <td>{{$tag->name}}</td>
+                                {{-- <td>{{$tag->posts->count()}}</td> --}}
+                                <td>
+                                    <a href="{{route('tags.edit',$tag->id)}}" class="btn btn-info btn-sm">Edit</a>
+                                </td>
+                                <td>
+                                    <form class="delete_form" action="{{route('tags.destroy',$tag->id)}}" method="post">
+                                        @csrf
+                                        <input type="hidden" name="_method" value="DELETE">
+                                        <input type="submit" name="" value="Delete" class="btn btn-danger btn-sm">
+                                    </form>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            @else
+                <h3 class="text-center">No Tag</h3>
+            @endif
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function(){
+            $('.delete_form').on('submit',function(){
+                if(confirm("Are you want to delete data?")){
+                    return true;
+                }else{
+                    return false;
+                }
+            })
+        })
+    </script>
+@endsection
+
+
