0fa443cf56018ef027fb10a6d95c9cf25a2d4731
P4TTANAPON <pattanaponn@gmail.com>
2022-07-25 21:04:03 +0700

    add pagination

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -51,6 +51,7 @@
                       </div>
                   @endforeach
                 </div>
+                {{$posts->links()}}
               </div>
 
               @include('layouts.sidebar')  
